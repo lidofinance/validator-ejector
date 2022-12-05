7bafa1784d83e927f473fc8b8c50ed5dccf91e7a
Eddort <webisforce@gmail.com>
2022-12-05 22:18:00 +0400

    fix: remove wrap from apis dto

--- src/lib/api/execution-dto.ts
+++ src/lib/api/execution-dto.ts
@@ -1,30 +1,28 @@
-import { arr, obj, str, wrap } from 'tooling-nanolib-test'
+import { arr, obj, str } from 'tooling-nanolib-test'
 
 export const lastBlockNumberDTO = (json: unknown) =>
-  wrap(
+  obj(
     json,
-    () =>
-      obj(json, (json) => ({
-        result: obj(json.result, (result) => ({
-          number: str(result.number, 'Invalid latest block number'),
-        })),
+    (json) => ({
+      result: obj(json.result, (result) => ({
+        number: str(result.number, 'Invalid latest block number'),
       })),
+    }),
     'Invalid validator LastBlockNumber response'
   )
 
 export const logsDTO = (json: unknown) =>
-  wrap(
+  obj(
     json,
-    () =>
-      obj(json, (json) => ({
-        result: arr(json.result, (result) =>
-          result.map((event) =>
-            obj(event, (event) => ({
-              data: arr(event.data, (data) => data.map(str)),
-              topics: arr(event.topics, (topics) => topics.map(str)),
-            }))
-          )
-        ),
-      })),
+    (json) => ({
+      result: arr(json.result, (result) =>
+        result.map((event) =>
+          obj(event, (event) => ({
+            data: arr(event.data, (data) => data.map(str)),
+            topics: arr(event.topics, (topics) => topics.map(str)),
+          }))
+        )
+      ),
+    }),
     'Empty data object from execution node for events'
   )
